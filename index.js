1f60e5321963c93b0e82c108a33e9f9a52dd9c69
MostakemHossain <mostakememon123@gmail.com>
2023-05-16 17:30:10 +0600

    update and delete api created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -24,6 +24,66 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollection = client.db("carDoctor").collection("services");
+
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const quary = { _id: new ObjectId(id) };
+      const options = {
+        projection: { _id: 1, title: 1, service_id: 1, price: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(quary, options);
+      res.send(result);
+    });
+
+    //bookings
+
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query.email);
+      let quary = {};
+      if (req.query?.email) {
+        quary = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(quary).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const uptadeBooking = req.body;
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) };
+      const uptadedDoc = {
+        $set: {
+          status: uptadeBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, uptadedDoc);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const quary = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(quary);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +91,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
