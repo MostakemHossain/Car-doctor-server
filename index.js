d9763917f75b05056d3285595fcdfeb8c642a05e
MostakemHossain <mostakememon123@gmail.com>
2023-05-17 19:48:10 +0600

    jwt verify added

--- index.js
+++ index.js
@@ -21,6 +21,29 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verify = (req, res, next) => {
+  console.log("hetting verify");
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  console.log(token);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SERECT, (error, decoded) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -57,8 +80,8 @@ async function run() {
 
     //bookings
 
-    app.get("/bookings", async (req, res) => {
-      console.log(req.query.email);
+    app.get("/bookings", verify, async (req, res) => {
+      // console.log(req.headers.authorization);
       let quary = {};
       if (req.query?.email) {
         quary = { email: req.query.email };
